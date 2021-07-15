721ae72c7e567fa0e4bffeeb96282d6bd48786fa
EspylArva <tchong.kite.h@gmail.com>
2021-07-15 22:05:23 +0200

    Added CSV reader

---
+++ app/src/main/java/com/iteration/kingdomino/csvreader/CSVReader.kt
@@ -0,0 +1,33 @@
+package com.iteration.kingdomino.csvreader
+
+import android.content.Context
+import com.iteration.kingdomino.R
+import com.iteration.kingdomino.game.Card
+import com.iteration.kingdomino.game.Tile
+import com.opencsv.CSVParserBuilder
+import com.opencsv.CSVReaderBuilder
+import timber.log.Timber
+import java.io.FileReader
+import java.io.InputStreamReader
+import java.util.*
+
+class CSVReader
+{
+    fun readCsv(context : Context) : Stack<Card>
+    {
+        val inputStream = context.resources.openRawResource(R.raw.dominos);
+        val listCards = Stack<Card>()
+        val csvParser = CSVParserBuilder().withSeparator(',').build()
+        val csvReader = CSVReaderBuilder(InputStreamReader(inputStream)).withCSVParser(csvParser).build()
+
+        var line: Array<String>? = csvReader.readNext()
+        while (line != null) {
+            // Do something with the data
+            val domino = Card(line[4].toInt(), Tile(Tile.Terrain.valueOf(line[1]), line[0].toInt()), Tile(Tile.Terrain.valueOf(line[3]), line[2].toInt()))
+            listCards.push(domino)
+            line = csvReader.readNext()
+        }
+
+        return listCards
+    }
+}
--- app/src/main/java/com/iteration/kingdomino/ui/home/HomeFragment.kt
+++ app/src/main/java/com/iteration/kingdomino/ui/home/HomeFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.iteration.kingdomino.R
+import com.iteration.kingdomino.csvreader.CSVReader
 import com.iteration.kingdomino.game.Card
 import com.iteration.kingdomino.game.Field
 import com.iteration.kingdomino.game.Player
@@ -21,7 +22,7 @@ class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
 
-    private var deck : Stack<Card> = Stack()
+    private lateinit var deck : Stack<Card>
     private lateinit var choice : List<Card>
     private lateinit var players : List<Player>
 
@@ -40,15 +41,18 @@ class HomeFragment : Fragment() {
 
         players = listOf(Player("John Doe"), Player("Emily Lee"), Player("Joseph Staline"), Player("Chris Tabernacle"))
 
+        deck = CSVReader().readCsv(requireContext())
+        deck.shuffle()
+
         // Template data
-        deck.add(Card(Tile(Tile.Terrain.FIELD, 2), Tile(Tile.Terrain.FOREST, 0)))
-        deck.add(Card(Tile(Tile.Terrain.FIELD, 1), Tile(Tile.Terrain.FOREST, 1)))
-        deck.add(Card(Tile(Tile.Terrain.FIELD, 1), Tile(Tile.Terrain.PLAIN, 2)))
-        deck.add(Card(Tile(Tile.Terrain.SEA, 2), Tile(Tile.Terrain.PLAIN, 0)))
-        deck.add(Card(Tile(Tile.Terrain.SEA, 1), Tile(Tile.Terrain.FOREST, 0)))
-        deck.add(Card(Tile(Tile.Terrain.SEA, 0), Tile(Tile.Terrain.FIELD, 1)))
-        deck.add(Card(Tile(Tile.Terrain.MINE, 2), Tile(Tile.Terrain.FIELD, 1)))
-        deck.add(Card(Tile(Tile.Terrain.MINE, 3), Tile(Tile.Terrain.FOREST, 0)))
+//        deck.add(Card(1, Tile(Tile.Terrain.FIELD, 2), Tile(Tile.Terrain.FOREST, 0)))
+//        deck.add(Card(2, Tile(Tile.Terrain.FIELD, 1), Tile(Tile.Terrain.FOREST, 1)))
+//        deck.add(Card(3, Tile(Tile.Terrain.FIELD, 1), Tile(Tile.Terrain.PLAIN, 2)))
+//        deck.add(Card(4, Tile(Tile.Terrain.SEA, 2), Tile(Tile.Terrain.PLAIN, 0)))
+//        deck.add(Card(5, Tile(Tile.Terrain.SEA, 1), Tile(Tile.Terrain.FOREST, 0)))
+//        deck.add(Card(6, Tile(Tile.Terrain.SEA, 0), Tile(Tile.Terrain.FIELD, 1)))
+//        deck.add(Card(7, Tile(Tile.Terrain.MINE, 2), Tile(Tile.Terrain.FIELD, 1)))
+//        deck.add(Card(8, Tile(Tile.Terrain.MINE, 3), Tile(Tile.Terrain.FOREST, 0)))
 
         choice = drawCards()
 
@@ -56,6 +60,8 @@ class HomeFragment : Fragment() {
 
         players[0].map.addTile(choice[0].tile1, Pair(3,4))
         players[0].debugPlayer()
+
+
 
 //         3 0
 //         0 0
