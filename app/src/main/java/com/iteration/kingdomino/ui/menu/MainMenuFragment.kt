6208c1a52067720056bd3d976c499f037d0c5f04
Tchong-Kite Huam <tchong.kite.h@gmail.com>
2022-09-14 22:50:47 +0200

    Adding navigation to Rules (example)

--- app/src/main/java/com/iteration/kingdomino/ui/menu/MainMenuFragment.kt
+++ app/src/main/java/com/iteration/kingdomino/ui/menu/MainMenuFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import com.iteration.kingdomino.R
+import timber.log.Timber
 
 class MainMenuFragment : Fragment() {
 
@@ -22,6 +25,14 @@ class MainMenuFragment : Fragment() {
         mainMenuViewModel =
                 ViewModelProvider(this).get(MainMenuViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_menu, container, false)
+
+        val btn_appendix = root.findViewById<Button>(R.id.btn_rules)
+
+        btn_appendix.setOnClickListener {
+            Timber.d("Navigating to appendix")
+            root.findNavController().navigate(R.id.nav_appendix)
+        }
+
         return root
     }
 }
