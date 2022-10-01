1247b63a3c319b84132ba30894b3b01ef781053c
Tchong-Kite Huam <tchong.kite.h@gmail.com>
2022-10-01 21:15:57 +0200

    Moving seed generation to NewGameBottomSheet, as it will contain the TextView

--- app/src/main/java/com/iteration/kingdomino/game/model/GameInfo.kt
+++ app/src/main/java/com/iteration/kingdomino/game/model/GameInfo.kt
@@ -1,30 +1,21 @@
 package com.iteration.kingdomino.game.model;
 import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
-import java.lang.reflect.Type
-import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalDateTime.now
-import kotlin.random.Random
 
 data class GameInfo(
     val gameId: Int,
     val players: Collection<Player>,
-    val modifiers: Set<String>) {
+    val modifiers: Set<String>,
+    var seed: Int ) {
 
     val deck: Set<Card> = HashSet()
     var choice: MutableList<Card> = mutableListOf()
 
-    val seed: Int = generateNewSeed()
     val creationDate: LocalDateTime = now()
     var lastUpdateDate: LocalDateTime = now()
-    var gameEnded: Boolean = false
 
-    private fun generateNewSeed(): Int {
-        // FIXME: get the list of seeds from DAO
-        val seeds = HashSet<Int>()
-        return sequence<Int> { (1.. 65536).random() }.first { !seeds.contains(it) }
-    }
+
 
     fun update() {
         // FIXME
