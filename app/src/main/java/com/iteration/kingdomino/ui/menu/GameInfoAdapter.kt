6dd1fb289316f96cc15c25965fcc0ca6f6291d15
Tchong-Kite Huam <tchong.kite.h@gmail.com>
2022-10-08 16:19:31 +0200

    Implementing LoadGameBottomSheet to inflate the layout file

--- app/src/main/java/com/iteration/kingdomino/ui/menu/GameInfoAdapter.kt
+++ app/src/main/java/com/iteration/kingdomino/ui/menu/GameInfoAdapter.kt
@@ -1,4 +1,30 @@
 package com.iteration.kingdomino.ui.menu
 
-class GameInfoAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.iteration.kingdomino.databinding.ViewholderGameInfoBinding
+import com.iteration.kingdomino.game.model.GameInfo
+
+class GameInfoAdapter(val gameInfos: List<GameInfo>) : RecyclerView.Adapter<GameInfoAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ViewholderGameInfoBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val gameInfo = gameInfos[position]
+
+        holder.binding.gameInfoSeed.editText!!.setText(gameInfo.seed.toString())
+        holder.binding.gameInfoUID.editText!!.setText(gameInfo.gameId.toString())
+        // TODO Chip?
+    }
+
+    override fun getItemCount(): Int = gameInfos.size
+
+    class ViewHolder(val binding: ViewholderGameInfoBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
 }
--- app/src/main/java/com/iteration/kingdomino/ui/menu/LoadGameBottomSheet.kt
+++ app/src/main/java/com/iteration/kingdomino/ui/menu/LoadGameBottomSheet.kt
@@ -1,6 +1,45 @@
 package com.iteration.kingdomino.ui.menu
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.iteration.kingdomino.components.RecyclerViewMargin
+import com.iteration.kingdomino.components.Utils
+import com.iteration.kingdomino.databinding.BottomsheetLoadGameBinding
+import com.iteration.kingdomino.game.data.DaggerProvider
+import com.iteration.kingdomino.game.data.DataManager
+import timber.log.Timber
+import javax.inject.Inject
 
 class LoadGameBottomSheet : BottomSheetDialogFragment() {
+
+    private var _binding: BottomsheetLoadGameBinding? = null
+    val binding get() = _binding!!
+
+    @Inject
+    lateinit var dataManager: DataManager
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = BottomsheetLoadGameBinding.inflate(inflater, container, false)
+
+        dataManager = DaggerProvider.create().dataManager()
+
+        setGameInfoRecycler()
+        return binding.root
+    }
+
+    private fun setGameInfoRecycler() {
+
+        val startedGames = dataManager.getStartedGames(requireContext())
+
+        Timber.d("Games loaded from shared preferences: $startedGames")
+
+        binding.currentGameRecycler.setHasFixedSize(true)
+        binding.currentGameRecycler.adapter = GameInfoAdapter(startedGames)
+        binding.currentGameRecycler.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        binding.currentGameRecycler.addItemDecoration(RecyclerViewMargin(Utils.pxToDp(2, requireContext()), 4))
+    }
 }
